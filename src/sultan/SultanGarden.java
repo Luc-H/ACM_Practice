4b36b0a574d5b51b0e9ae27149689ad85a78c2df
Luc Hayward <luchay10@gmail.com>
2017-09-09 14:08:03 +0200

    Test

---
+++ src/sultan/SultanGarden.java
@@ -0,0 +1,28 @@
+package sultan;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class SultanGarden
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		//Input
+		//Test cases
+		int T = input.nextInt();
+		String[] cases = new String[T];
+		for(int i=0;i<T;i++)
+		{
+			cases[i]=input.next();
+		}
+
+
+
+
+	}
+
+
+}
