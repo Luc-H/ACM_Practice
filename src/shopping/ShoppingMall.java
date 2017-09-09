b3549c3892746d09602eedf4db9aec1013c158d4
Luc Hayward <luchay10@gmail.com>
2017-09-09 10:41:40 +0200

    Shopping mall addidtions

--- src/shopping/ShoppingMall.java
+++ src/shopping/ShoppingMall.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class ShoppingMall
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Node[] nodes;
 		Edge[] edges;
 
@@ -14,19 +15,21 @@ public class ShoppingMall
 		// n= num nodes		m = num edges
 		int n = input.nextInt(), m = input.nextInt();
 		nodes = new Node[n];
-		edges = new Edge[m];
+		edges = new Edge[m * 2];
 
 		//get input for nodes
-		for (int i = 0; i < n; i++) {
-			nodes[i] = new Node(input.nextInt(), input.nextInt(), input.nextInt());
+		for (int i = 0; i < n; i++)
+		{
+			nodes[i] = new Node(input.nextInt(), input.nextInt(), input.nextInt(), i);
 		}
 		//get input for edges
-		for (int i = 0; i < m; i++) {
+		for (int i = 0; i < m * 2; i += 2)
+		{
 			int s = input.nextInt(), e = input.nextInt();
 			String t = input.next();
 			// define two edges each for bi-directionality
 			edges[i] = new Edge(nodes[s], nodes[e], t, false);
-			edges[i] = new Edge(nodes[e], nodes[s], t, true);
+			edges[i + 1] = new Edge(nodes[e], nodes[s], t, true);
 		}
 
 		input.nextLine();
@@ -34,7 +37,8 @@ public class ShoppingMall
 		int q = Integer.parseInt(input.nextLine());
 		int[][] queries = new int[q][2];
 		//get the queries
-		for (int i = 0; i < q; i++) {
+		for (int i = 0; i < q; i++)
+		{
 			queries[i][0] = input.nextInt();
 			queries[i][1] = input.nextInt();
 		}
@@ -42,16 +46,114 @@ public class ShoppingMall
 		//***************
 		System.out.println("Input complete");
 
-		//Dijkstra
+		for (int i = 0; i < queries.length; i++)
+		{
+			dijkstraS(nodes, edges, nodes[queries[i][0]], nodes[queries[i][1]]);
+		}
+
+	}
+
+
+	private static void dijkstraL(Node[] nodes, Edge[] edges, Node origin, Node destination)
+	{
+		//Reset all nodes
+		for (Node node : nodes)
+		{
+			node.distanceFromSource = Double.MAX_VALUE;
+			node.checked = false;
+		}
+
+		//Set origin to our initial node.
+		origin.distanceFromSource = 0;
+		origin.checked = true;
+
+		//create and empty array of nodes visited
+		Node[] visitedNodes = new Node[nodes.length];
+		boolean uncheckedNodes = true;
+		Node currentNode = origin;
+
+
+
+	}
+
+
+	private static void dijkstraS(Node[] nodes, Edge[] edges, Node origin, Node destination)
+	{
+		double[] distances = new double[edges.length];
+		for (int i = 0; i < nodes.length; i++)
+		{
+			nodes[i].checked = false;
+			nodes[i].distanceFromSource = 1000000;
+		}
+		origin.distanceFromSource = 0;
+		origin.checked = true;
+
+		Node currentNode = origin;
+
+		currentNode.distanceFromSource = 0;
+
+		boolean uncheckedNodes = true;
+		for (int i = 0; i < 100; i++)
+		{
+			for (Edge edge : edges)
+			{
+				if (edge.from != currentNode || edge.to.checked) continue;
+
+				if (currentNode.distanceFromSource + edge.weight < edge.to.distanceFromSource)
+				{
+					edge.to.distanceFromSource = currentNode.distanceFromSource + edge.weight;
+					edge.to.from = currentNode;
+				}
+				currentNode.checked = true;
+
+				uncheckedNodes = false;
+				for (Node node : nodes)
+				{
+					if (!node.checked)
+					{
+						if (!uncheckedNodes)
+						{
+							currentNode = node;
+							uncheckedNodes = true;
+						}
+
+						if (node.distanceFromSource < currentNode.distanceFromSource)
+						{
+							currentNode = node;
+						}
+					}
+				}
+			}
+
+
+		}
+
+		currentNode = destination;
+		String path = currentNode.number + "";
+
+		while (currentNode != origin)
+		{
+			currentNode = currentNode.from;
+			path = currentNode.number + " " + path;
+		}
+
+		System.out.println(path);
+
 	}
 
 	/**
 	 * Holds the information for a node
 	 */
-	static class Node {
+	private static class Node
+	{
 		int floor;
 		int x;
 		int y;
+
+		double distanceFromSource;
+		boolean checked;
+		Node from;
+		int number;
 
 		/**
 		 * Nodes Constructor
@@ -60,19 +162,22 @@ public class ShoppingMall
 		 * @param x     the x-pos
 		 * @param y     the y-pos
 		 */
-		Node(int floor, int x, int y) {
+		Node(int floor, int x, int y, int number)
+		{
 			this.x = x;
 			this.y = y;
 			this.floor = floor;
+			this.number = number;
 		}
 	}
 
 	/**
 	 * Holds the information for an edge
 	 */
-	static class Edge {
-		Node start;
-		Node end;
+	private static class Edge
+	{
+		Node from;
+		Node to;
 		double weight;
 
 		/**
@@ -83,9 +188,10 @@ public class ShoppingMall
 		 * @param weightType The type of edge weighting (ie: stairs/walking/lift/escalator)
 		 * @param reverse    Escalators have alternate reverse weights, used to determine which type.
 		 */
-		Edge(Node start, Node end, String weightType, Boolean reverse) {
-			this.start = start;
-			this.end = end;
+		Edge(Node start, Node end, String weightType, Boolean reverse)
+		{
+			this.from = start;
+			this.to = end;
 			this.weight = calculateWeight(weightType, start, end, reverse);
 		}
 
@@ -98,18 +204,25 @@ public class ShoppingMall
 		 * @param reverse    Escalators have alternate reverse weights, used to determine which type.
 		 * @return The final weighting after calculations
 		 */
-		private static double calculateWeight(String weightType, Node start, Node end, Boolean reverse) {
-			if (weightType.equals("walking")) {
+		private static double calculateWeight(String weightType, Node start, Node end, Boolean reverse)
+		{
+			if (weightType.equals("walking"))
+			{
 				return Math.sqrt(Math.pow(start.x - end.x, 2) + Math.pow(start.y - end.y, 2));
-			} else if (weightType.equals("stairs")) {
+			} else if (weightType.equals("stairs"))
+			{
 				return Math.sqrt(Math.pow(Math.sqrt(Math.pow(start.x - end.x, 2) + Math.pow(start.y - end.y, 2)), 2) + 25);
-			} else if (weightType.equals("lift")) {
+			} else if (weightType.equals("lift"))
+			{
 				return 1;
-			} else {
-				if (reverse) {
+			} else
+			{
+				if (reverse)
+				{
 					return 3 * Math.sqrt(Math.pow(Math.sqrt(Math.pow(start.x - end.x, 2) + Math.pow(start.y - end.y, 2)), 2) + 25);
 
-				} else {
+				} else
+				{
 					return 1;
 				}
 
